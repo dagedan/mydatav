dc370a21b9d5df066ae69e22c607eeff2227965e
shenshuping <184341327@qq.com>
2022-06-20 23:33:05 +0800

    尝试动画点

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Scene, LineLayer, PolygonLayer } from '@antv/l7';
+import { Scene, LineLayer, PolygonLayer, Popup, PointLayer } from '@antv/l7';
 // import { GaodeMap } from '@antv/l7-maps';
 import { GaodeMapV2 } from '@antv/l7-maps'; // 默认引入高德2.0 版本
 import data from '../../data/area.json';
@@ -8,7 +8,7 @@ const Map = () => {
     const scene = new Scene({
       id: 'map',
       map: new GaodeMapV2({
-        pitch: 35.210526315789465,
+        // pitch: 35.210526315789465,
         mapStyle: 'amap://styles/grey',
         center: [99.828225, 23.472719],
         zoom: 9,
@@ -31,7 +31,74 @@ const Map = () => {
           'rgb(8,81,156)',
         ])
         .shape('fill');
+      //  图层边界
+      const chinaBorderLineLayer = new LineLayer({
+        zIndex: 2,
+      })
+        .source(data)
+        .color('rgb(93,112,146)')
+        .size(0.6);
+
+      const selectLineLayer = new LineLayer({
+        zIndex: 2,
+      })
+        .source(emptyFeatureCollextion)
+        .color('#fff')
+        .size(2);
+
+      const boxLayer = new PolygonLayer({})
+        .source(emptyFeatureCollextion)
+        .color('#fff')
+        .size(2)
+        .style({
+          opacity: 0.6,
+          lineType: 'dash',
+          dashArray: [5, 5],
+        })
+        .shape('line');
+
+      chinaPolygonLayer.on('unclick', () => {
+        selectLineLayer.setData(emptyFeatureCollextion);
+      });
+
       scene.addLayer(chinaPolygonLayer);
+      scene.addLayer(chinaBorderLineLayer);
+      scene.addLayer(selectLineLayer);
+      scene.addLayer(boxLayer);
+      chinaPolygonLayer.on('mousemove', (e) => {
+        const popup = new Popup({
+          offsets: [0, 0],
+          closeButton: false,
+        })
+          .setLnglat(e.lngLat)
+          .setHTML(
+            `<span style="color:red">${e.feature.properties.name}</span>`,
+          );
+        scene.addPopup(popup);
+      });
+
+      fetch('/public/points.csv')
+        .then((res) => res.text())
+        .then((data) => {
+          const pointLayer = new PointLayer({})
+            .source(data, {
+              parser: {
+                type: 'csv',
+                x: 'Longitude',
+                y: 'Latitude',
+              },
+            })
+            .shape('circle')
+            .active(true)
+            .animate(true)
+            .size(56)
+            .color('#4cfd47')
+            .style({
+              opacity: 1,
+            });
+
+          scene.addLayer(pointLayer);
+        });
     });
   }, []);
 
