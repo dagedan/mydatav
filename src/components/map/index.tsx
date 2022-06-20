b4e91d38d153e0e5201016e709f61766ec6da352
shenshuping <184341327@qq.com>
2022-06-20 22:59:35 +0800

    '绘制双江县的底图'

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,22 +1,37 @@
 import React, { useState, useEffect } from 'react';
-import { Scene } from '@antv/l7';
+import { Scene, LineLayer, PolygonLayer } from '@antv/l7';
 // import { GaodeMap } from '@antv/l7-maps';
 import { GaodeMapV2 } from '@antv/l7-maps'; // 默认引入高德2.0 版本
-import area from '../../data/area.json';
+import data from '../../data/area.json';
 const Map = () => {
-  const [list, setData] = useState([]);
-
   useEffect(() => {
     const scene = new Scene({
       id: 'map',
       map: new GaodeMapV2({
         pitch: 35.210526315789465,
         mapStyle: 'amap://styles/grey',
-        center: [104.288144, 31.239692],
-        zoom: 4.4,
+        center: [99.828225, 23.472719],
+        zoom: 9,
         token: '3f730ff87182dd4ccf1446f014d94592',
       }),
       logoVisible: false,
+    });
+    const emptyFeatureCollextion = {
+      type: 'FeatureCollection',
+      features: [],
+    };
+    scene.on('loaded', () => {
+      const chinaPolygonLayer = new PolygonLayer()
+        .source(data)
+        .color('name', [
+          'rgb(239,243,255)',
+          'rgb(189,215,231)',
+          'rgb(107,174,214)',
+          'rgb(49,130,189)',
+          'rgb(8,81,156)',
+        ])
+        .shape('fill');
+      scene.addLayer(chinaPolygonLayer);
     });
   }, []);
 
